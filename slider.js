ca7cb497311294525c9f563d2f2213f46240ad00
Mateo Enrico <mateoenrico1221@gmail.com>
2023-11-28 20:21:14 -0300

    slider de dos fotos

--- slider.js
+++ slider.js
@@ -2,28 +2,30 @@ const slides = document.querySelector('.slides');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 let slideIndex = 0;
+let slideWidth = slides.children[0].offsetWidth;
 let autoSlideInterval;
 
 prevBtn.addEventListener('click', () => {
-  slideIndex = (slideIndex === 0) ? slides.childElementCount - 1 : slideIndex - 1;
+  slideIndex = (slideIndex === 0) ? slides.childElementCount / 2 - 1 : slideIndex - 1;
   updateSlidePosition();
   resetAutoSlide();
 });
 
 nextBtn.addEventListener('click', () => {
-  slideIndex = (slideIndex === slides.childElementCount - 1) ? 0 : slideIndex + 1;
+  slideIndex = (slideIndex === slides.childElementCount / 2 - 1) ? 0 : slideIndex + 1;
   updateSlidePosition();
   resetAutoSlide();
 });
 
 function updateSlidePosition() {
-  const slideWidth = slides.children[0].offsetWidth;
+  slideWidth = slides.children[0].offsetWidth;
+  slides.style.transition = 'transform 0.5s ease-in-out';
   slides.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
 }
 
 function autoSlide() {
   autoSlideInterval = setInterval(() => {
-    slideIndex = (slideIndex === slides.childElementCount - 1) ? 0 : slideIndex + 1;
+    slideIndex = (slideIndex === slides.childElementCount / 2 - 1) ? 0 : slideIndex + 1;
     updateSlidePosition();
   }, 5000);
 }
