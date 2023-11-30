a27853a39ed3c7ae46a4e88b8d6e584a908b4f9d
Mateo Enrico <mateoenrico1221@gmail.com>
2023-11-30 10:57:05 -0300

    agrego despliegue de barra en celular con estilo

--- desplegar.js
+++ desplegar.js
@@ -4,12 +4,5 @@ const navbar = document.getElementById('navbar');
 
 // Agregar un evento de clic al botón
 menuBtn.addEventListener('click', function() {
-  const isOpen = navbar.classList.contains('show-menu');
-  
-  // Verificar si el menú está abierto
-  if (isOpen) {
-    navbar.classList.remove('show-menu'); // Si está abierto, ciérralo
-  } else {
-    navbar.classList.add('show-menu'); // Si está cerrado, ábrelo
-  }
+  navbar.classList.toggle('show-menu');
 });
